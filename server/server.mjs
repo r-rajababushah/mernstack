c41e5e09002eee1ef692d5b042d1b6393e75a465
Rajababu Shah <rajababushah.in@gmail.com>
2023-07-23 20:27:00 +0530

    done

--- server/server.mjs
+++ server/server.mjs
@@ -10,19 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/record", records);
 
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, "client", "build")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-        });
-    })
-}
-
-
 // Start the Express Server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
